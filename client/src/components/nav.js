e8c557e76baa40d0f08234e405271c40f582ed13
John Iversen <john.e.iversen@outlook.com>
2018-04-09 11:52:26 +0200

    just a sec

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 
 class Nav extends Component {
-  render(
-    <nav>
-      <ul>
-        <li>Leaderboard</li>
-        <li>About</li>
-      </ul>
-    </nav>
-  );
-};
+  // constructor(props) {
+  //   super(props);
+  //
+  //   this.state = {isToggleOn: false};
+  //   this.handleClick = this.handleClick.bind(this);
+  // };
+
+  // handleClick() {
+  //   this.setState(isToggleOn => ({
+  //     t
+  //   }));
+  //   console.log('this is:', this);
+  // };
+
+  render() {
+    return (
+        // <div>
+        // <nav>
+        //   <ul>
+        //     <li className="dropdown-content">Leaderboard</li>
+        //     <li className="dropdown-content">About</li>
+        //   </ul>
+        // </nav>
+        // <div className="leaderboard" onClick={document.querySelector(".leaderboard").style.display = "block"}></div>
+        // <div className="about" onClick={document.querySelector(".about").style.display = "block"}></div>
+        // </div>
+      );
+    };
+  };
 
 export default Nav;
