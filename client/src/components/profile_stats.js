ade7cba8364ec1eaab96d3f6409b0689a45d0706
Adam Hermansson <qurridutt@gmail.com>
2018-04-09 11:56:00 +0200

    added react-materialize library

--- client/src/components/profile_stats.js
+++ client/src/components/profile_stats.js
@@ -1,3 +1,21 @@
 import React, { Component } from 'react';
 
+
+class ProfileStats extends Component {
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {statItem}
+        </ul>
+      </div>
+    );
+  }
+}
+
 export default ProfileStats;
