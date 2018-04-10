ffc85ce5e6c43a49169299e783e26dcb43005ad0
Felix Morau <felixmorau@gmail.com>
2018-04-10 16:40:38 +0200

    does this work for all?

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -10,7 +10,7 @@ class Header extends Component {
         return (
           <div>
               <img className="headerImage" src="style/images/calle.jpg"></img>
-              <div classname="headerH1"><h1>___Politweet</h1></div>
+              <div classname="headerH1"><h1>Politweet</h1></div>
               <div classname="headerH2"><h2>Choose your fighters</h2></div>
           </div>
         );
