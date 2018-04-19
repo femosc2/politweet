3a623a5d40a1d7825f3cbf7af05a75dd8327996f
Adam Nilsson <AdamNilsson_@hotmail.com>
2018-04-19 14:00:37 +0200

    Fixed stat_item.js

--- client/src/components/stat_item.js
+++ client/src/components/stat_item.js
@@ -43,41 +43,6 @@ class StatItem extends Component {
         <div className="statItem"> {wordCount} </div>
       </li>
 
-    if (this.props.selectedFighter === null) {
-      return (<div id="hiddenLoading">Loading</div>)
-    }
-
-    // Chooses the JSON file for the selected fighter
-    const statItem = require("../../tweets/" + this.props.selectedFighter + ".json");
-
-    // TODO make a list instead of a bunch of const"s
-    const image = statItem.image;
-    const name = statItem.name;
-    const description = statItem.description;
-    const followers = statItem.followers;
-    const friends = statItem.friends;
-    const quotes = statItem.quotes;
-    const mentions = statItem.mentions;
-    const party = statItem.party;
-    const age = statItem.age;
-    const accCreation = statItem.accCreation;
-    const wordCount = statItem.wordCount;
-
-    return (
-        <li id="statList">
-          <div className="statItem"> {image} </div>
-          <div className="statItem"> {name} </div>
-          <div className="statItem"> {description} </div>
-          <div className="statItem"> {followers} </div>
-          <div className="statItem"> {friends} </div>
-          <div className="statItem"> {quotes} </div>
-          <div className="statItem"> {mentions} </div>
-          <div className="statItem"> {party} </div>
-          <div className="statItem"> {age} </div>
-          <div className="statItem"> {accCreation} </div>
-          <div className="statItem"> {wordCount} </div>
-        </li>
-
     );
   }
 }
