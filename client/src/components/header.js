71efecc99c1757987099075291b4c1fd1cc59bd6
Victor Persson <vp-96@hotmail.com>
2018-04-25 14:47:05 +0200

    Font/h1 fix

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -7,12 +7,12 @@ class Header extends Component {
       <header>
         <img className="headerImage" src="/images/headerTest.png" alt="Carl Bildt" />
         <div className="headerH1">
-          <h1>Politweet</h1>
+          <h1> ☭ Politweet ☭ </h1>
         </div>
 
         <div className="headerH2">
 
-          <h2>Choose your fighters</h2>
+          <h2>Choose your beta leftist fighters!</h2>
         </div>
       </header>
     );
