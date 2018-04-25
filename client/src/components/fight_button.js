0863aeeccf952bbcae1302a6a4cd1c4292242366
Adam Nilsson <AdamNilsson_@hotmail.com>
2018-04-25 13:30:33 +0200

    fixed fightbutton hover

--- client/src/components/fight_button.js
+++ client/src/components/fight_button.js
@@ -5,7 +5,22 @@ class FightButton extends Component {
   render() {
     const buttonText = this.props.buttonText;
     return (
-      <a className="waves-effect waves-light btn-large"> {buttonText} </a>
+
+        <ul className="modalButton">
+          <ul id="nav-mobile" className="">
+            <a className="waves-effect waves-light btn-large modal-trigger" href="#modal3"> {buttonText} </a>
+            <div id="modal3" className="modal">
+              <div className="modal-content">
+                <h4 className="modalHeader">Battle</h4>
+                <p>Statcontainers and fighterprofiles</p>
+              </div>
+              <div className="modal-footer">
+                <a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat">Close</a>
+              </div>
+            </div>
+          </ul>
+        </ul>
+
     );
   }
 }
