331354c48b6fec6ff6e3f0267ee2e33f65f38ecb
Felix Morau <felixmorau@gmail.com>
2018-05-14 15:41:16 +0200

    added all twitter accounts to word_count with John Iversen

--- client/tweets/wordcount.py
+++ client/tweets/wordcount.py
@@ -11,6 +11,22 @@ def annie():
         tweets.append(line)
     print(len(tweets))
     print(len(word_count))
+
+def get_word_count(account):
+    minigolf = open(account+"tweets.json", "r")
+    lines = minigolf.readlines()
+    word_count = []
+    tweets = []
+    for line in lines:
+        tweets.append(line)
+    print(len(tweets),  "words has", account, "written!")
             
     
-annie()
+get_word_count("@annieloof")
+get_word_count("@bjorklundjan")
+get_word_count("@BuschEbba")
+get_word_count("@carlbildt")
+get_word_count("@IsabellaLovin")
+get_word_count("@jimmieakesson")
+get_word_count("@jsjostedt")
+get_word_count("@SwedishPM")
