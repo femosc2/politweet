1edef5fb8b907fa1ee00f2e8ff384a9ab3f3e870
Adam Nilsson <AdamNilsson_@hotmail.com>
2018-05-14 19:07:25 +0200

    Changed modals, structure and content

--- client/src/components/modals.js
+++ client/src/components/modals.js
@@ -15,9 +15,13 @@ class Modals extends Component {
 						<a className="waves-effect waves-light btn modal-trigger" href="#modal1">About</a>
 						<div id="modal1" className="modal">
 							<div className="modal-content">
-								<h4 className="modalHeader">About</h4>
-								<p>Ett gäng goa'gubbar bestående av: Victor Persson, Felinux Morau,
-								John Iversen, Adam Hermansson samt Adam Nilsson</p>
+							<h5 className="modalHeader">The creators of Politweet:</h5>
+							<p>A couple of goa'gubbar currently studying Information Architecture at Malmö University consisting of
+								 Adam Hermansson, Adam Nilsson, Felix Morau,
+								John Iversen, and THE horunge Victor Persson.</p>
+							<h5 className="modalHeader">Politweet's purpose:</h5>
+							<p>Jackass warning text - det här har inget att göra med verkligheten, bara på skoj. Låt eller låt INTE
+							denna hemsida påverka dina framtida politiska val.</p>
 							</div>
 							<div className="modal-footer">
 								<a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat">Close</a>
@@ -33,27 +37,49 @@ class Modals extends Component {
 								<table>
 									<thead>
 										<tr>
-											<th>Namn</th>
 											<th>Plats</th>
+											<th>Namn</th>
+											<th>Antal vinster</th>
+											<th>Antal förluster</th>
 											<th>Procent win</th>
 										</tr>
 									</thead>
 
 									<tbody>
 										<tr>
-											<td>Annie Lööf</td>
 											<td>1</td>
-											<td>72.5%</td>
-										</tr>
-										<tr>
-											<td>Stefan Löfven</td>
-											<td>2</td>
-											<td>50%</td>
-										</tr>
-										<tr>
-											<td>Jimmie Åkesson</td>
+											<td>Annie Lööf</td>
+											<td>7</td>
 											<td>3</td>
-											<td>46.5%</td>
+											<td>70%</td>
+										</tr>
+										<tr>
+											<td>2</td>
+											<td>Stefan Löfven</td>
+										<td>6</td>
+										<td>4</td>
+										<td>60%</td>
+										</tr>
+										<tr>
+											<td>3</td>
+											<td>Jimmie Åkesson</td>
+										<td>5</td>
+										<td>4</td>
+										<td>55.5%</td>
+										</tr>
+										<tr>
+											<td>4</td>
+											<td>Jonas Sjöstedt</td>
+										<td>3</td>
+										<td>3</td>
+										<td>50%</td>
+										</tr>
+										<tr>
+											<td>5</td>
+											<td>Ebba Busch Thor</td>
+											<td>2</td>
+											<td>4</td>
+											<td>33.3%</td>
 										</tr>
 									</tbody>
 								</table>
