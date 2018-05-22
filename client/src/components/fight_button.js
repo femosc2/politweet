786a56b3bb67b1e1baae7091a18e22a2285dc92a
johniversen <john.e.iversen@outlook.com>
2018-05-22 16:50:39 +0200

    adam h, adam n, victor

--- client/src/components/fight_button.js
+++ client/src/components/fight_button.js
@@ -1,24 +1,33 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "../styles/fight_button.css";
 
 class FightButton extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			wordCount: []
+		}
+	}
+
 	render() {
-		const buttonText = this.props.buttonText;
-		const selectedFighter = this.props.selectedFighter;
+		const wordCountFileReader = require("./fight.json")
+		console.log(wordCountFileReader);
+
 		return (
 				<ul className="modalButton">
 					<ul id="nav-mobile" className="">
-						<a className="waves-effect waves-light btn-large modal-trigger" href="#modal3"> {buttonText} </a>
+						<a className="waves-effect waves-light btn-large modal-trigger" href="#modal3"> {this.props.buttonText} </a>
 					<div id="modal3" className="modal fightContentHeight">
 							<div className="modal-content fightContainer">
 								<h4 className="modalHeader">Batalj</h4>
 								<p>Här ska det slåss!!!!!!!!!</p>
-								<h4> {selectedFighter} </h4>
-								{	console.log({selectedFighter})}
-							</div>
+								<h4> {this.props.selectedFighter} </h4>
 							<div className="modal-footer">
 								<a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat">Stäng</a>
 							</div>
+						</div>
 						</div>
 					</ul>
 				</ul>
