3cd241d843071d1df9e24945d848b11845699592
Victor Persson <vp-96@hotmail.com>
2018-05-23 16:51:23 +0200

    Rebuilt & completed fight_button.js, a winner can now be calculated & announced

--- client/src/components/fight_button.js
+++ client/src/components/fight_button.js
@@ -10,33 +10,86 @@ class FightButton extends Component {
 			)
 		}
 
-		const fighter1 = this.props.stats[0].followers;
-		const fighter2 = this.props.stats[1].followers;
+		const fighter1Followers = this.props.stats[0].followers;
+		const fighter2Followers = this.props.stats[1].followers;
 
-		console.log(fighter1);
-		console.log(fighter2);
+		const fighter1Friends = this.props.stats[0].friends;
+		const fighter2Friends = this.props.stats[1].friends;
 
-		return (
-			<div>
-				<ul className="modalButton" id="show-modal-ul">
-					<ul id="nav-mobile" className="">
-						<a className="btn-large pulse modal-trigger" href="#modal3" id="show-modal3"> KÄBBEL </a>
-					<div id="modal3" className="modal fightContentHeight">
-							<div className="modal-content fightContainer">
-								<h4 className="modalHeader">Batalj</h4>
-								<p>Här ska det slåss!!!!!!!!!</p>
-								<h4> Följare för ditt första val: {fighter1}</h4>
-								<h4> Följare för ditt andra val: {fighter2}</h4>
-							<div className="modal-footer">
-								<a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat">Stäng</a>
+		const fighter1StatusesCount = this.props.stats[0].statusesCount;
+		const fighter2StatusesCount = this.props.stats[1].statusesCount;
+
+		const fighter1 = this.props.stats[0].name;
+		const fighter2 = this.props.stats[1].name;
+
+		const totalPointsFighter1 = Math.floor((fighter1Followers + fighter1Friends + fighter1StatusesCount) / 10000)
+		const totalPointsFighter2 = Math.floor((fighter2Followers + fighter2Friends + fighter2StatusesCount) / 10000)
+
+		if (totalPointsFighter1 > totalPointsFighter2) {
+			return (
+				<div>
+					<ul className="modalButton" id="show-modal-ul">
+						<ul id="nav-mobile" className="">
+							<a className="btn-large pulse modal-trigger" href="#modal3" id="show-modal3"> KÄBBEL </a>
+						<div id="modal3" className="modal fightContentHeight">
+								<div className="modal-content fightContainer">
+									<h4 className="modalHeader">Batalj</h4>
+								<h5> Vinnaren är: {fighter1}! </h5>
+
+								<div className="modal-footer">
+									<a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat">Stäng</a>
+								</div>
 							</div>
-						</div>
-						</div>
+							</div>
+						</ul>
 					</ul>
-				</ul>
-			</div>
-		);
+				</div>
+			);
+
+		} else if (totalPointsFighter1 === totalPointsFighter2) {
+			return (
+				<div>
+					<ul className="modalButton" id="show-modal-ul">
+						<ul id="nav-mobile" className="">
+							<a className="btn-large pulse modal-trigger" href="#modal3" id="show-modal3"> KÄBBEL </a>
+						<div id="modal3" className="modal fightContentHeight">
+								<div className="modal-content fightContainer">
+									<h4 className="modalHeader">Batalj</h4>
+								<h5> Bara käbbel! Det blev lika mellan {fighter1} och {fighter2}! </h5>
+
+								<div className="modal-footer">
+									<a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat">Stäng</a>
+								</div>
+							</div>
+							</div>
+						</ul>
+					</ul>
+				</div>
+			);
+
+		} else {
+			return (
+				<div>
+					<ul className="modalButton" id="show-modal-ul">
+						<ul id="nav-mobile" className="">
+							<a className="btn-large pulse modal-trigger" href="#modal3" id="show-modal3"> KÄBBEL </a>
+						<div id="modal3" className="modal fightContentHeight">
+								<div className="modal-content fightContainer">
+									<h4 className="modalHeader">Batalj</h4>
+								<h4> Vinnaren är: {fighter2}! </h4>
+
+								<div className="modal-footer">
+									<a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat">Stäng</a>
+								</div>
+							</div>
+							</div>
+						</ul>
+					</ul>
+				</div>
+			);
+		};
+
+		}
 	}
-}
 
 export default FightButton;
