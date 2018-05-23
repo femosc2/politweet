ae632c1f87a3049df447cc4a1b5718492827f1eb
Victor Persson <vp-96@hotmail.com>
2018-05-23 17:43:16 +0200

    Fixed file paths, imgs works

--- client/src/components/roster.js
+++ client/src/components/roster.js
@@ -10,48 +10,48 @@ class Roster extends Component {
 			<div className="row">
 				<RosterProfile
 					selectFighter={this.props.selectFighter}
-				img="images/annie_lööf.png" id="fighter1" alt="Annie Lööf"
+				img="images/fighter1.png" id="fighter1" alt="Annie Lööf"
 				/>
 
 				<RosterProfile
 					selectFighter={this.props.selectFighter}
-					img="images/stefan_löfven.png" id="fighter2" alt="Stefan Löfven"
-				/>
-			</div>
-
-			<div className="row">
-				<RosterProfile
-					selectFighter={this.props.selectFighter}
-					img="images/carl_bildt.png" id="fighter3" alt="Carl Bildt"
-				/>
-
-				<RosterProfile
-					selectFighter={this.props.selectFighter}
-					img="images/jonas_sjöstedt.png" id="fighter4" alt="Jonas Sjöstedt"
+					img="images/fighter2.png" id="fighter2" alt="Stefan Löfven"
 				/>
 			</div>
 
 			<div className="row">
 				<RosterProfile
 					selectFighter={this.props.selectFighter}
-					img="images/jimmie_åkesson.png" id="fighter5" alt="Jimmie Åkesson"
+					img="images/fighter3.png" id="fighter3" alt="Carl Bildt"
 				/>
 
 				<RosterProfile
 					selectFighter={this.props.selectFighter}
-					img="images/jan_björklund.png" id="fighter6" alt="Jan Björklund"
+					img="images/fighter4.png" id="fighter4" alt="Jonas Sjöstedt"
 				/>
 			</div>
 
 			<div className="row">
 				<RosterProfile
 					selectFighter={this.props.selectFighter}
-					img="images/ebba_busch_thor.png" id="fighter7" alt="Ebba Busch Thor"
+					img="images/fighter5.png" id="fighter5" alt="Jimmie Åkesson"
 				/>
 
 				<RosterProfile
 					selectFighter={this.props.selectFighter}
-					img="images/isabella_lövin.png" id="fighter8" alt="Isabella Lövin"
+					img="images/fighter6.png" id="fighter6" alt="Jan Björklund"
+				/>
+			</div>
+
+			<div className="row">
+				<RosterProfile
+					selectFighter={this.props.selectFighter}
+					img="images/fighter7.png" id="fighter7" alt="Ebba Busch Thor"
+				/>
+
+				<RosterProfile
+					selectFighter={this.props.selectFighter}
+					img="images/fighter8.png" id="fighter8" alt="Isabella Lövin"
 				/>
 			</div>
 		</div>
