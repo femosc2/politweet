f479bb1c4a5a4fb732fc0aad50b4d8dd0d847f3b
Adam Nilsson <AdamNilsson_@hotmail.com>
2018-05-23 11:06:01 +0200

    Added spacing for statcontainer text

--- client/src/components/fight_button.js
+++ client/src/components/fight_button.js
@@ -2,19 +2,37 @@ import React, { Component } from "react";
 import "../styles/fight_button.css";
 
 class FightButton extends Component {
+/*
+	averageWord() {
+		const wordCountFileReader = require("../../tweets/" + this.props.selectedFighter + ".json");
+
+		if (this.state.statusesCount > 3200) {
+			const wordCount = wordCountFileReader.wordCount / 3200;
+			return wordCount;
+		}
+
+		else {
+			const wordCount = wordCountFileReader.wordCount / this.state.statusesCount;
+			return wordCount;
+		}
+	}
+
+	<div className="statItem"
+	title="Räknas ut genom att dela totala antalet ord politkern använt med hur många statusar den gjort.">
+	Antal ord per tweet i genomsnitt: {Math.floor(this.averageWord())}
+	</div>
+
+*/
 	render() {
-		const buttonText = this.props.buttonText;
-		const selectedFighter = this.props.selectedFighter;
 		return (
 				<ul className="modalButton">
 					<ul id="nav-mobile" className="">
-						<a className="waves-effect waves-light btn-large modal-trigger" href="#modal3"> {buttonText} </a>
+						<a className="waves-effect waves-light btn-large modal-trigger" href="#modal3"> {this.props.buttonText} </a>
 					<div id="modal3" className="modal fightContentHeight">
 							<div className="modal-content fightContainer">
 								<h4 className="modalHeader">Batalj</h4>
 								<p>Här ska det slåss!!!!!!!!!</p>
-								<h4> {selectedFighter} </h4>
-								{	console.log({selectedFighter})}
+							<h4> {this.props.selectedFighter} </h4>
 							</div>
 							<div className="modal-footer">
 								<a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat">Stäng</a>
