89ee6454a593bbbcc48f60c2396a083b9988eeb5
Adam Nilsson <AdamNilsson_@hotmail.com>
2018-05-23 11:33:12 +0200

    Made the logo bigger and changed the margin from the corner

--- client/src/components/fight_button.js
+++ client/src/components/fight_button.js
@@ -3,30 +3,6 @@ import axios from "axios";
 import "../styles/fight_button.css";
 
 class FightButton extends Component {
-<<<<<<< HEAD
-/*
-	averageWord() {
-		const wordCountFileReader = require("../../tweets/" + this.props.selectedFighter + ".json");
-
-		if (this.state.statusesCount > 3200) {
-			const wordCount = wordCountFileReader.wordCount / 3200;
-			return wordCount;
-		}
-
-		else {
-			const wordCount = wordCountFileReader.wordCount / this.state.statusesCount;
-			return wordCount;
-		}
-	}
-
-	<div className="statItem"
-	title="Räknas ut genom att dela totala antalet ord politkern använt med hur många statusar den gjort.">
-	Antal ord per tweet i genomsnitt: {Math.floor(this.averageWord())}
-	</div>
-
-*/
-	render() {
-=======
 	constructor() {
 		super();
 
@@ -39,7 +15,6 @@ class FightButton extends Component {
 		const wordCountFileReader = require("./fight.json")
 		console.log(wordCountFileReader);
 
->>>>>>> 5a2c0fb121e5b131ff01eea61ebd83f751930998
 		return (
 				<ul className="modalButton">
 					<ul id="nav-mobile" className="">
@@ -48,17 +23,12 @@ class FightButton extends Component {
 							<div className="modal-content fightContainer">
 								<h4 className="modalHeader">Batalj</h4>
 								<p>Här ska det slåss!!!!!!!!!</p>
-<<<<<<< HEAD
-							<h4> {this.props.selectedFighter} </h4>
-							</div>
-=======
 								<h4> {this.props.selectedFighter} </h4>
->>>>>>> 5a2c0fb121e5b131ff01eea61ebd83f751930998
+							</div>
 							<div className="modal-footer">
 								<a href="#!" className="modal-action modal-close waves-effect waves-green btn-flat">Stäng</a>
 							</div>
-						</div>
-						</div>
+					</div>
 					</ul>
 				</ul>
 		);
